c2c412459c3efb905e394a21718ca3fb22d5fa4e
Adazii <kaka.barbu@gmail.com>
2023-12-15 18:28:37 +0100

    ...

--- Server.js
+++ Server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors()); // Enable CORS for all routes
 
-app.use('/getFootballData', createProxyMiddleware({
-    target: 'https://api.football-data.org',
-    changeOrigin: true,
-    headers: {
-        'X-Auth-Token': '5157b19281c649c0b30aa052e8b96fe1', // Updated with your actual API key
+app.get('/getFootballData', async (req, res) => {
+  try {
+    const apiUrl = 'https://api.football-data.org//v4/competitions/CL/matches'; // Replace with the actual API endpoint
+
+    const response = await fetch(apiUrl, {
+      headers: {
+        'X-Auth-Token': '5157b19281c649c0b30aa052e8b96fe1', // Replace with your actual API key
         'Content-Type': 'application/json',
-    },
-}));
+      },
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
